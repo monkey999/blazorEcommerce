8d6e3917fed07277ceb7436d0da6922e2ef99f1c
tanosorda <omegaga1999@gmail.com>
2023-11-15 20:01:35 +0200

    73. Implement a CartService on the Client

--- BlazorEcommerce/Client/Program.cs
+++ BlazorEcommerce/Client/Program.cs
@@ -2,6 +2,7 @@ global using BlazorEcommerce.Client.Services.CategoryService;
 global using BlazorEcommerce.Client.Services.ProductService;
 global using BlazorEcommerce.Shared;
 global using System.Net.Http.Json;
+using BlazorEcommerce.Client.Services.CartService;
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -20,6 +21,7 @@ namespace BlazorEcommerce.Client
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<IProductService, ProductService>();
             builder.Services.AddScoped<ICategoryService, CategoryService>();
+            builder.Services.AddScoped<ICartService, CartService>();
 
             await builder.Build().RunAsync();
         }
---
+++ BlazorEcommerce/Client/Services/CartService/CartService.cs
@@ -0,0 +1,36 @@
+﻿using Blazored.LocalStorage;
+
+namespace BlazorEcommerce.Client.Services.CartService
+{
+    public class CartService : ICartService
+    {
+        private readonly ILocalStorageService _localStorageService;
+
+        public CartService(ILocalStorageService localStorageService)
+        {
+            _localStorageService = localStorageService;
+        }
+
+        public event Action OnChange;
+
+        public async Task AddToCart(CartItem cartItem)
+        {
+            var cart = await _localStorageService.GetItemAsync<List<CartItem>>("cart");
+
+            cart ??= new List<CartItem>();
+
+            cart.Add(cartItem);
+
+            await _localStorageService.SetItemAsync("cart", cart);
+        }
+
+        public async Task<List<CartItem>> GetCartItems()
+        {
+            var cart = await _localStorageService.GetItemAsync<List<CartItem>>("cart");
+
+            cart ??= new List<CartItem>();
+
+            return cart;
+        }
+    }
+}
