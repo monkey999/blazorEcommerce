29ab19cb4b93e6412c8ac75b21e1a40f1c290ab9
tanosorda <omegaga1999@gmail.com>
2023-12-14 03:29:47 +0200

    106. Implement the Login on the Client

--- BlazorEcommerce/Client/Services/AuthService/IAuthService.cs
+++ BlazorEcommerce/Client/Services/AuthService/IAuthService.cs
@@ -1,7 +1,10 @@
-﻿namespace BlazorEcommerce.Client.Services.AuthService
+﻿
+
+namespace BlazorEcommerce.Client.Services.AuthService
 {
     public interface IAuthService
     {
         Task<ServiceResponse<int>> Register(UserRegister request);
+        Task<ServiceResponse<string>> Login(UserLogin request);
     }
 }
